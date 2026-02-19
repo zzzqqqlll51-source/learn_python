367b6eb8b1e5a6302c537742abc5ecb591ed43be
Liu Ziqi <zzzqqqlll51@gmail.com>
2026-02-19 11:41:26 +0800

    2.19

---
+++ p036_chinese_jieba.py
@@ -0,0 +1,13 @@
+
+content = """
+春姑娘悄悄地来到了我们的校园!
+绿油油的小草争着抢着从地下探出头来，东张西望地看着四周。池塘边的柳树，抽出了新的柳枝和柳叶，微风轻轻一吹，柳树就晃动着自己的秀发。
+花园里各种各样的花儿都开了，
+有艳红的玫瑰花、粉色的桃花、雪白的梨花......五颜六色，美不胜收。
+"""
+
+import jieba
+import re
+content = re.sub(r"[\s。，......！、]","",content)
+wordlist = jieba.lcut(content)
+print(wordlist)
