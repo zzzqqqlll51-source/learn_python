60da20eabf1e04a8e8322842842932b7527ead3d
Liu Ziqi <zzzqqqlll51@gmail.com>
2026-02-25 21:54:20 +0800

    2.25

---
+++ Data Container.py
@@ -0,0 +1,155 @@
+# # Calculate the average, maximum, and minimum values ​​of ten numbers entered by the user.
+# list1 = []
+# for i in range(1,11):
+#     list1.append(int(input(f"input the {i} item:")))
+
+# print(list1)
+# list1.sort()
+# print(list1)
+# print(list1[0])
+# print(list1[-1])
+# avg = sum(list1) / len(list1)
+# print(avg)
+
+
+# # Merge two lists and remove duplicates
+# num_list1 = [19, 23, 54, 64, 875, 20, 109, 232, 123, 54]
+# num_list2 = [55, 80, 72, 35, 60, 123, 54, 29, 91]
+
+# num_list1 = num_list1 + num_list2
+
+# print(num_list1)
+
+# newlist = []
+# for num in num_list1:
+#     if num not in newlist:
+#         newlist.append(num) 
+
+# print(newlist)
+
+
+# 选修足球学生名单
+football_set = {"王林","曾牛","徐立国","遁天","天运子","韩立","厉飞雨","乌丑","紫灵"}
+#选修篮球学生名单
+basketba_set = {"张铁","墨居仁","王林","姜老道","曾牛","王蝉","韩立","天运子","李化元","厉飞雨","云露"}
+#选修法语学生名单
+french_set = {"许木","王卓","十三","虎咆","姜老道","天运子","红蝶","厉飞雨","韩立","曾牛"}
+# 选修艺术学生名单
+art_set = {"遁天","天运子","韩立","虎咆","姜老道","紫灵"}
+#1.找出同时选修了法语和艺术的学生
+# Method 1
+fa_set = french_set.intersection(art_set)
+print(fa_set)
+
+# Method 2
+fa_set = french_set & art_set
+print(fa_set)
+
+#2.找出同时选修了所有四门课程的学生
+all_set = football_set & basketba_set & french_set & art_set
+print(all_set)
+
+
+#3.找出选修了足球,但是没有选修篮球的学生
+# Method 1 
+f_de_b_set = football_set.difference(basketba_set)
+print(f_de_b_set)
+
+# Method 2
+f_de_b_set = football_set - basketba_set
+print(f_de_b_set)
+
+# Method 3
+f_de_b_set = {i for i in football_set if i not in basketba_set}
+print(f_de_b_set)
+
+
+# 4.统计每一个学生选修的课程数量
+stu_set = football_set | basketba_set | french_set | art_set
+
+# for student in stu_set:
+#     cnt = 0
+#     if student in football_set:
+#         cnt += 1
+#     if student in basketba_set:
+#         cnt += 1
+#     if student in french_set:
+#         cnt += 1
+#     if student in art_set:
+#         cnt += 1
+#     print(f"{student} : {cnt}")
+
+stu_list = [*football_set,*basketba_set,*french_set,*art_set]
+for student in stu_set:
+    print(f"{student}:{stu_list.count(student)}")
+
+
+
+
+# 开发一个购物车管理系统，实现商品信息的添加、修改、删除、查询功能。
+# 系统使用字典结构存储商品数据，通过控制台菜单与用户交互。具体功能如下:
+# 1.添加购物车:用户根据提示录入商品名称、以及该商品的价格、数量，保存该商品信息到购物车。
+# 2.修改购物车:要求用户输入要修改的购物车商品名称，然后再提示输入该商品的价格、数量，输入完成后修改该商品信息。
+# 3.删除购物车:要求用户输入要删除的购物车名称，根据名称删除购物车中的商品。
+# 4.查询购物车:将购物车中的商品信息展示出来，格式为:"商品名称:xxx，商品价格:xxx，商品数量:xxx"。
+# 5.退出购物车
+
+shopping_cart = {}
+
+# make a manu
+print("Welcome to the shopping cart managing system ~")
+manu = """
+############## shopping cart system #############
+#              1. Add to cart                   # 
+#              2. Edit cart                     #
+#              3. Delete cart                   # 
+#              4. View cart                     #
+#              5. Exit cart                     #
+"""
+print(manu)
+
+# Perform specific operations
+while True:
+    choice = input("please input your choice hear:")
+    match manu:
+        case "1":
+            name = input("please input the good\'s name:")
+            price = float(input("please input the price of the goods:"))
+            quantity = input("please input the quantity of goods:")
+
+            # If it already exists, the addition will not be performed, and a prompt message will be displayed.
+            if name in shopping_cart:
+                print("the goods has aleady exists!")
+            else:
+                shopping_cart[name] = {"price":price,"quantity":quantity}
+
+        case "2":
+            name = input("please input the good you want to edit:")
+            if name in shopping_cart:
+                new_price = float(input("please input the new price:"))
+                new_quantity = input("please input the new quantity:")
+                shopping_cart[name] = {"price":new_price,"quantity":new_quantity}
+            else:
+                print("This product does not exist!")
+
+        case "3":
+            name = input("please input the good you want to delete")
+            if name in shopping_cart:
+                shopping_cart.pop(name)
+            else:
+                price("This product does not exist!")
+            
+        case "4":
+            for item in shopping_cart.items():
+                print(item)
+
+        case "5":
+            print("bye~")
+            break
+
+        case _:
+            print("Illegal operation, not supported!")
+
+
+
+
--- P02_judge.py
+++ P02_judge.py
@@ -19,11 +19,121 @@
 
 
 
-# judge a number
-number = int(input("please input a number:"))
-if number > 0:
-    print(">0")
-elif number < 0:
-    print("<0")
-else:
-    print("=0")
+# # judge a number
+# number = int(input("please input a number:"))
+# if number > 0:
+#     print(">0")
+# elif number < 0:
+#     print("<0")
+# else:
+#     print("=0")
+
+
+# judge a day
+# day = input("please input a day:")
+
+# match day:
+#     case "1":
+#         print("Monday")
+#     case "2":
+#         print("Tuesday")
+#     case "3":
+#         print("Wednesday")
+#     case "4":
+#         print("Thursday")
+#     case "5":
+#         print("Friday")
+#     case "6"|"7":
+#         print("Weekend")
+#     case _:
+#         print("error")
+
+
+# # print words for 10 times
+# i = 0
+# while(i < 10):
+#     print("aaa")
+#     i += 1
+# else:
+#     print("The end.The cycling worked well.")
+
+
+# #The sum of even numbers within 1-100
+# i = 1
+# total = 0
+# while i <= 100:
+#     if i % 2 == 0:
+#         total += i
+#     i += 1
+
+# print(f"total = {total}")
+
+
+# # try to use for
+# msg = input("please input a message:")
+# for word in msg:
+#     print(f"word:{word}")
+# else:
+#     print("The end.The cycling worked well.")
+
+
+# # try to use range
+# total = 0
+# for i in range(102,501,3):
+#     total += i
+# else:
+#     print(f"total = {total}")
+
+
+
+# # Nested Loops: Print Rectangles
+# m = int(input("please input the lenth:"))
+# n = int(input("please input the width:"))
+# for i in range(n):
+#     for k in range(m):
+#         print("*  ",end="")
+#     print("\n")
+
+
+# # Print the multiplication table
+# for i in range(1,10):
+#     for k in range(1,i+1):
+#         print(f"{k} * {i} = {i * k}",end="\t")
+#     print("\n")
+
+
+# # Make the user log in repeatedly until successful.
+
+# def islanded(username,password):
+#     if (username == "admin" and password == "666888") or (username == "zhangsan" and password == "123456"):
+#         print("Login successful!")
+#         return True
+#     elif username == "" or password == "":
+#         print("Username or password cannot be empty, please re-enter.")
+#         return False
+#     else:
+#         print("Username or password incorrect, please re-enter.")
+#         return False
+
+# while True:
+#     username = input("input your username:")
+#     password = input("input your password:")
+#     if(islanded(username,password)):
+#         break
+
+
+# Number Guessing Game
+import random
+answer = random.randint(a=0,b=100)
+while True:
+    num = int(input("please input a number:"))
+    
+    if num > answer:
+        print("the number is too big!")
+        continue
+    elif num < answer:
+        print("the number is too small!")
+        continue
+    else:
+        print(f"correct!the correct number is {answer}.")
+        break
